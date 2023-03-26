a99ab5015c9f522f21bc109158b2f3fb0995a832
Hazrat-Ali7 <programmer.hazratali@gmail.com>
2023-03-26 16:05:15 +0600

    program added

---
+++ userinput.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class userinput {
+    public static void main(String args[])
+    {
+               int a;
+               System.out.println("Enter Number = ");
+               Scanner S = new Scanner(System.in);
+               a=S.nextInt();
+               System.out.println(a);
+
+    }
+    
+}
